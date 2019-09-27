0485d3b40241603977dcfc924248570fd18574d4
denaldo98 <denaldo98@gmail.com>
2019-09-27 15:58:35 +0200

    Start progetto

---
+++ src/main/java/com/denapoli/progettoop/ProgettoopApplication.java
@@ -0,0 +1,13 @@
+package com.denapoli.progettoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoopApplication.class, args);
+	}
+
+}
