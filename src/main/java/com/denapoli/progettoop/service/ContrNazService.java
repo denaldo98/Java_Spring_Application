baa1fec0f528e047fe3db9d4e3d6ce74bd561bc7
denaldo98 <denaldo98@gmail.com>
2019-10-03 17:26:19 +0200

    aggiunti metodi in Service per calcolo statistiche dopo filtraggio; inserimento commenti

--- src/main/java/com/denapoli/progettoop/service/ContrNazService.java
+++ src/main/java/com/denapoli/progettoop/service/ContrNazService.java
@@ -27,7 +27,7 @@ import java.util.*;
 @Service
 public class ContrNazService {
     private final static String COMMA_DELIMITER = ";"; //separatore CSV
-    public static List<String> anni=new ArrayList<>();
+    public static List<String> anni=new ArrayList<>(); //predispongo lista di stringhe per controllo campo contributi con anno
     private List<ContributoNazione> contributi = new ArrayList<>();     //lista di istanze della classe modellante
     public Metadata metadata;           //metadata
 
@@ -78,8 +78,8 @@ public class ContrNazService {
                 e.printStackTrace ();
             }
         parsing ( fileCSV ); //effettuo il parsing
-        metadata = new Metadata (); //costruttore classe Metadata
-    } //fine costruttore
+        metadata = new Metadata (); //costruttore classe Metadata->generiamo i metadati in modo da restituirli quando richiesto
+    }
 
 
     /**
@@ -100,18 +100,13 @@ public class ContrNazService {
      *
      * @param fileCSV file del quale effettuare il parsing
      */
-
-
-
     private void parsing(String fileCSV) {
-        try ( BufferedReader bffr = new BufferedReader ( new FileReader ( fileCSV ) ) ) {
+        try ( BufferedReader bffr = new BufferedReader ( new FileReader ( fileCSV ) ) ) { //inizializzo buffer per il parsing
             bffr.readLine (); // salto la prima riga poichè contiene intestazione
             String riga;
             while ((riga = bffr.readLine ()) != null) {    // leggo ogni riga del file
-                //sostituisco le virgole con ; che utilizzerò come separatore
-                riga = riga.replace ( ",", COMMA_DELIMITER );
-                //uso split per dividere la riga in corrispondenza dei separatori, con trim elimino i caratteri non visibili
-                String[] rigaSeparata = riga.trim ().split ( COMMA_DELIMITER );
+                riga = riga.replace ( ",", COMMA_DELIMITER ); //sostituisco le virgole con ; che utilizzerò come separatore
+                String[] rigaSeparata = riga.trim ().split ( COMMA_DELIMITER ); //uso split per dividere la riga in corrispondenza dei separatori, con trim elimino i caratteri non visibili
                 // prendiamo i valori dei singoli campi dalla riga
                 char freq = rigaSeparata[0].trim ().charAt ( 0 );//freq è di tipo char
                 String geo = rigaSeparata[1].trim ();
@@ -123,13 +118,14 @@ public class ContrNazService {
                 }
                 // prendendo i valori ottenuti dal parsing, creo un nuovo oggetto e lo inserisco nella lista
                 ContributoNazione nuova = new ContributoNazione ( freq, geo, unit, aid_instr, contributo );
-                contributi.add ( nuova );
+                contributi.add ( nuova ); //inserimento in lista
             }
-        }// apro il buffer di lettura
+        }
         catch (IOException e) {
             e.printStackTrace ();
         }
     }
+
         /**
      * Restituisce il dataset completo
      *
@@ -146,7 +142,7 @@ public class ContrNazService {
      * @return l'oggetto corrispondente al valore di indice n
      */
     public ContributoNazione getContrNaz(int n) {//restituisce il contributo n-esimo
-        if (n < contributi.size()) return contributi.get(n);
+        if (n < contributi.size()) return contributi.get(n); //controllo che l'indice non superi la dimensione della lista
         throw new ResponseStatusException ( HttpStatus.BAD_REQUEST, "Oggetto di indice " + n + " non esiste!");
     }
 
@@ -157,7 +153,7 @@ public class ContrNazService {
      * @return Map contenente le statistiche
      */
     public Map getStatistiche(String fieldName) {
-        return  Statistiche.getTutteStatistiche(fieldName, getFieldValues(fieldName));
+        return  Statistiche.getTutteStatistiche(fieldName, getValoriCampo (fieldName));
     }
 
     /**
@@ -165,13 +161,12 @@ public class ContrNazService {
      *
      * @return lista di mappe contenenti le statistiche relative ad ogni campo
      */
-
     public List<Map> getStatistiche() {
-        Field[] fields = ContributoNazione.class.getDeclaredFields();// questo ci da l'elenco di tutti gli attributi della classe
-        List<Map> list = new ArrayList<>();
+        Field[] fields = ContributoNazione.class.getDeclaredFields();// otteniamo l'elenco di tutti gli attributi della classe
+        List<Map> list = new ArrayList<>(); //inizializzo lista di mappe che conterrà le statistiche
         for (Field f : fields) {
             String fieldName = f.getName();//f è l'oggetto di tipo fieldsName estrae il nome del campo corrente
-            if(fieldName.equals("contributo"))
+            if(fieldName.equals("contributo")) //gestione vettore di double contributo
                 for( int i=0; i<ContributoNazione.intervalloAnni; i++)
                     list.add(getStatistiche(Integer.toString(2000+i) ));
                 else list.add(getStatistiche(fieldName));//va ad aggiungere alla lista  la mappa che contiene le statistiche del campo fieldName
@@ -180,40 +175,41 @@ public class ContrNazService {
         return list;
     }
 
-
     /**
-     * Metodo che estrae dalla lista di oggetti la lista dei valori relativi ad un singolo campo: se si tratta del campo contributi(vettore di double) viene richiesto come parametro anche l'anno
+     * Metodo che estrae dalla lista di oggetti la lista dei valori relativi ad un singolo campo del dataset
      *
-     * @param nomeCampo campo del quale estrarre i valori
+     * @param nomeCampo campo del dataset del quale estrarre i valori(eventuale anno se si vuole contributo)
      * @return lista dei valori del campo richiesto
      */
-
-    //dovrebbe andare
-    private List getFieldValues(String nomeCampo) {
-        List<Object> values = new ArrayList<>();
+    private List getValoriCampo(String nomeCampo) {
+        List<Object> values = new ArrayList<>(); //inizializzo lista che conterrà i valori del campo
         try {
-            if(!anni.contains(nomeCampo)){
-            //serve per scorrere tutti gli oggetti ed estrarre i valori del campo nomeCampo
-                for (ContributoNazione contr : contributi) {
-                    Method getter = ContributoNazione.class.getMethod("get" + nomeCampo.substring(0, 1).toUpperCase() + nomeCampo.substring(1));
-                    Object value = getter.invoke(contr);
+            //caso in cui nomeCampo sia un anno: verifico che sia uno degli anni gestiti
+            if(anni.contains(nomeCampo)){
+                for(ContributoNazione contr : contributi){
+                    Object value= contr.getContributo()[Integer.parseInt(nomeCampo)-2000]; //considero solo l'elemento che mi interessa del metodo get
                     values.add(value);
                 }
             }
+            //caso in cui nomeCampo non sia un anno
             else {
-                    for(ContributoNazione contr : contributi){
-                        Object value= contr.getContributo()[Integer.parseInt(nomeCampo)-2000];
-                        values.add(value);
-            }
+                //serve per scorrere tutti gli oggetti ed estrarre i valori del campo nomeCampo
+                for (ContributoNazione contr : contributi) {
+                    Method getter = ContributoNazione.class.getMethod("get" + nomeCampo.substring(0, 1).toUpperCase() + nomeCampo.substring(1)); //costruisco il metodo get del modello di riferimento
+                    Object value = getter.invoke(contr); //invoco il metodo get sull'oggetto della classe modellante
+                    values.add(value); //aggiungo il valore alla lista
+                }
+
             }
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Field '" + nomeCampo + "' does not exist");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Il campo '" + nomeCampo + "' non esiste!");
         } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
-        return values;
+        return values; //ritorno la lista
     }
+
     /**
      * Restituisce una lista formata dagli oggetti che soddisfano il filtro
      *
@@ -222,15 +218,55 @@ public class ContrNazService {
      * @param rif valore di riferimento
      * @return lista di oggetti che soddisfano il filtro
      */
-
-    //da modificare
     public List<ContributoNazione> getDatiFiltrati(String nomeCampo, String oper, Object rif) {
-        List<Integer> filtrati = Filtri.filtra(getFieldValues(nomeCampo), oper, rif);    //applico il filtro alla lista
-        List<ContributoNazione> risultatoFiltro = new ArrayList<>(); //aggiungo alla lista solo gli oggetti che soddisfano le specifiche del filtro attraverso gli indici
+        List<Integer> filtrati = Filtri.filtra( getValoriCampo (nomeCampo), oper, rif);    //applico il filtro alla lista di valori rispetto al campo nomeCampo
+        List<ContributoNazione> risultatoFiltro = new ArrayList<>(); //inizializzo lista che conterrà i dati filtrati
+        //aggiungo alla lista solo gli oggetti che soddisfano le specifiche del filtro attraverso gli indici
         for (int i : filtrati) {
             risultatoFiltro.add(contributi.get(i));
         }
         return risultatoFiltro;
     }
 
+    /**
+     * Metodo che, applicando un filtro, restituisce le statistiche relative a tutti i campi
+     *
+     * @param campoFiltro campo sul quale si applica il filtro
+     * @param oper operatore di confronto
+     * @param rif valore di riferimento
+     * @return lista delle mappe contenenti le statistiche di ogni campo
+     */
+    public List<Map> getStatisticheFiltrate(String campoFiltro, String oper, Object rif) {
+        Field[] fields = ContributoNazione.class.getDeclaredFields ();// ottengo l'elenco di tutti gli attributi della classe modellante
+        List<Map> lista = new ArrayList<> (); //inizializzo la lista delle mappe
+        for (Field f : fields) {
+            String nomeCampo = f.getName ();//f è l'oggetto di tipo field, con getName si estrae il nome del campo corrente
+            if(nomeCampo.equals ("contributo")) //gestione campo contributo con anni
+                for(int i=0; i<ContributoNazione.intervalloAnni; i++)
+                    lista.add( getStatisticheFiltrate ( Integer.toString(i+2000), campoFiltro, oper, rif )); //calcolo statistiche per ogni anno e aggiungo la mappa alla lista
+                else //gestione altri campi
+                lista.add ( getStatisticheFiltrate ( nomeCampo, campoFiltro, oper, rif ) );//calcolo statistiche per altri campo e aggiungo la mappa alla lista
+        }
+        return lista; //ritorno la lista di mappe ottenuta
+    }
+        /**
+     * Metodo che, applicando un filtro, restituisce le statistiche relative ad un campo
+     *
+     * @param campoStatistiche campo del quale si richiedono le statistiche
+     * @param campoFiltro campo sul quale si applica il filtro
+     * @param oper operatore di confronto
+     * @param rif valore di riferimento
+     * @return Mappa con le statistiche sul campo passato come primo parametro applicate solo agli elementi che soddisfano il filtro
+     */
+    public Map getStatisticheFiltrate(String campoStatistiche, String campoFiltro, String oper, Object rif) {
+        List<Integer> indici = Filtri.filtra( getValoriCampo (campoFiltro), oper, rif); //indici degli elementi che soddisfano il filtro
+        List valoriCampo = getValoriCampo (campoStatistiche); //lista dei valori del campo rispetto al quale calcolare le statistiche
+        List<Object> filtrati = new ArrayList<>(); //inizializzo lista che conterrà gli elementi filtrati
+        for (int i : indici) {
+            filtrati.add(valoriCampo.get(i)); // aggiungo alla lista gli elementi che soddisfano il filtro
+        }
+        return Statistiche.getTutteStatistiche (campoStatistiche, filtrati); //calcolo e ritorno le statistiche
+    }
+
 }
+
