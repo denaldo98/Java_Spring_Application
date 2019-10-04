b262673fc8a77182a01420380a97968ad380b919
S1082351 <S1082351@studenti.univpm.it>
2019-10-04 14:26:50 +0200

    metodi post con filtri

--- src/main/java/com/denapoli/progettoop/controller/ContrNazController.java
+++ src/main/java/com/denapoli/progettoop/controller/ContrNazController.java
@@ -3,9 +3,11 @@ package com.denapoli.progettoop.controller;
 import com.denapoli.progettoop.modello.ContributoNazione;
 import com.denapoli.progettoop.service.ContrNazService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.BasicJsonParser;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -88,6 +90,73 @@ public class ContrNazController {
             return lista;
         }
     }
+
+
+
+    /**
+     * Metodo per eseguire il parsing del filtro passato tramite body di una POST
+     *
+     * @param body body della richiesta POST contenente un filtro
+     * @return mappa contenente i parametri del filtro: campo, operatore, valore di riferimento
+     */
+    private static Map<String, Object> parseFiltro(String body) {
+        Map<String, Object> bodyParsato = new BasicJsonParser().parseMap(body);
+        String nomeCampo = bodyParsato.keySet().toArray(new String[0])[0];
+        Object valore = bodyParsato.get(nomeCampo);
+        Object rif;
+        String oper;
+        if (valore instanceof Map) {
+            Map filtro = (Map) valore;
+            oper = ((String) filtro.keySet().toArray()[0]).toLowerCase();
+            rif = filtro.get(oper);
+        } else {
+            oper = "$eq";
+            rif = valore;
+        }
+        Map<String, Object> filtro = new HashMap<>();
+        filtro.put("oper", oper);
+        filtro.put("campo", nomeCampo);
+        filtro.put("rif", rif);
+        return filtro;
+    }
+
+    /**
+     * Metodo che gestisce una richiesta POST alla rotta "/data", resituendo la lista dei record che soddisfano il filtro
+     *
+     * @param body body della richiesta POST contenente il filtro
+     * @return lista di oggetti che soddisfano il filtro
+     */
+    @PostMapping("/data")
+    public List getDatiFiltrati(@RequestBody String body) {
+        Map<String, Object> filtro = parseFiltro(body);
+        String nomeCampo = (String) filtro.get("campo");
+        String oper = (String) filtro.get("oper");
+        Object rif = filtro.get("rif");
+        return service.getDatiFiltrati(nomeCampo, oper, rif);
+    }
+
+    /**
+     * Metodo che gestisce la richiesta POST alla rotta "/statistiche", restiuisce le statistiche sul campo richiesto (opzionale) o su tutti i campi, considerando soltanto i record che soddisfano il filtro
+     *
+     * @param fieldName campo di cui si richiedono le statistiche (opzionale)
+     * @param body      body della richiesta POST che contiene il filtro
+     * @return lista contenente le statistiche richieste
+     */
+    @PostMapping("/statistiche")
+    public List<Map> getStatisticheFiltrate(@RequestParam(value = "campo", required = false, defaultValue = "") String fieldName, @RequestBody String body) {
+        Map<String, Object> filtro = parseFiltro(body);
+        String campoFiltro = (String) filtro.get("field");
+        String oper = (String) filtro.get("oper");
+        Object rif = filtro.get("rif");
+        if (fieldName.equals("")) {
+            return service.getStatisticheFiltrate(campoFiltro, oper, rif);
+        } else {
+            List<Map> lista = new ArrayList<>();
+            lista.add(service.getStatisticheFiltrate(fieldName, campoFiltro, oper, rif));
+            return lista;
+        }
+    }
+
 
 
 
